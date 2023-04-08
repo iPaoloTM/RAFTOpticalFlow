54dde8c86b1e74d89c36f5d2307d464158095e5e
Paolo <43711362+iPaoloTM@users.noreply.github.com>
2023-04-08 20:27:42 +0200

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,4 +1,5 @@
+#change the upper limit of the sequence with the actual number of frames of the video
 for i in $(seq 0 47)
 do
-    mv flow${i}.png flow${i}-$((i+1)).png 
+    mv flow${i}-$((i+1)).png flow${i}.png 
 done
