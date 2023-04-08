6cf849b87a1230cea3643b1c73b4de34f6ecad4f
Paolo <43711362+iPaoloTM@users.noreply.github.com>
2023-04-08 18:58:47 +0200

    Update runner_optiflow.sh
    hint

--- runner_optiflow.sh
+++ runner_optiflow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#change the upper limit of the sequence with the actual number of frames of the video
 for i in $(seq 0 47)
 do
   echo "python3 optiflow.py --frame1 $i --frame2 $(($i+1))"
